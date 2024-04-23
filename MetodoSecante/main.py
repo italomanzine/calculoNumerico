b2df198d98f4ed0df7359c5ffb064ade29b14e2b
Italo Manzine <italo.mazine@contractor.rhimagnesita.com>
2024-04-23 08:12:21 -0300

    Refactor main.py to use sympy for symbolic calculations and improve user input handling

--- MetodoSecante/main.py
+++ MetodoSecante/main.py
@@ -1,8 +1,18 @@
-from function import f
+import sympy as sp
 from secant_method import secant_method, plot_secant_method
 
+# Defina a variável simbólica
+x = sp.symbols('x')
+
 def main():
-    print("Método da Secante para encontrar a raiz de uma função.")
+    # Entrada do usuário para a função
+    # Exemplo: Para uma função f(x) = x^2 + x - 6
+    # O usuário deve digitar 'x**2 + x - 6'
+    function_input = input("Digite a função que deseja encontrar a raiz, usando 'x' como variável: ")
+    f_expr = sp.sympify(function_input)  # Converte a entrada do usuário em uma expressão sympy
+    f = sp.lambdify(x, f_expr, 'numpy')  # Converte a expressão sympy para uma função numpy
+
+    # Continuação do código para ler os outros parâmetros e executar o método da secante
     x0 = float(input("Digite o primeiro valor inicial x0: "))
     x1 = float(input("Digite o segundo valor inicial x1: "))
     tol = float(input("Digite a tolerância desejada: "))
